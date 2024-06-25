6197bba243ab1e04fd408770024fdd4448f911cb
jreyes1990 <jarl1990mt@gmail.com>
2024-06-24 22:05:46 -0600

    19. Triangulo de pascal

--- Alex Joel Pagoada Suazo/cursoNetCore/Program.cs
+++ Alex Joel Pagoada Suazo/cursoNetCore/Program.cs
@@ -8,7 +8,8 @@ class Program
     Program progra = new Program();
     // progra.functionTipoDato(separador);
     // progra.functionArray(separador);
-    progra.functionCicloFor(separador);
+    // progra.functionCicloFor(separador);
+    progra.trianguloPascal(separador);
   }
 
   public void functionTipoDato(String separador)
@@ -110,6 +111,50 @@ class Program
     foreach (var item in name)
     {
       Console.WriteLine("Nombre: {0}", item);
+    }
+
+    Console.ReadLine();
+  }
+
+  public void trianguloPascal(String separador)
+  {
+    Console.WriteLine(separador + separador + separador + separador);
+    Console.WriteLine("Triangulo de Pascal:");
+    int pisos = 0;
+    int[] arreglo = new int[1];
+    // Mostramos un mensaje y capturamos el dato ingresado desde la consola
+    Console.WriteLine("Ingrese el numero de pisos: ");
+    pisos = Convert.ToInt16(Console.ReadLine());
+    /* Declaramos el primer ciclo for que va a recorrer dependiendo 
+       el dato ingresado que está almacenado en la variable pisos */
+    for (int i = 1; i <= pisos; i++)
+    {
+      /* Colocamos un arreglo y la variable i del ciclo for que será el tamaño
+         que tendrá el arreglo, cada vez que el ciclo for se ejecute */
+      int[] pascal = new int[i];
+      // Ciclo for que se decrementa para formar el triangulo
+      for (int j = pisos; j < i; j++)
+      {
+        Console.WriteLine(" ");
+      }
+      // Ciclo for que genera la suma de las cifras
+      for (int k = 0; k < i; k++)
+      {
+        // Condicion que evalúa la variable del ciclo for
+        if (k == 0 || k == (i - 1))
+        {
+          pascal[k] = 1;
+        }
+        else
+        {
+          /* Sumamos los numeros que estan en cada posicion del arreglo
+             para formar el triangulo */
+          pascal[k] = arreglo[k] + arreglo[k - 1];
+        }
+        Console.Write("[" + pascal[k] + "]");
+      }
+      arreglo = pascal;
+      Console.WriteLine(" ");
     }
 
     Console.ReadLine();
