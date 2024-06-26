eab642b026a7d2015b3da549eccb726bd9f2508e
jreyes1990 <jarl1990mt@gmail.com>
2024-06-25 21:32:40 -0600

    31. Modificador private y métodos que no retorna elementos

---
+++ Alex Joel Pagoada Suazo/AppNetCore/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AppNetCore;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    var data = new Program();
+    data.metodoPrivado();
+  }
+
+  private void metodoPrivado()
+  {
+    Console.WriteLine("Hello, World!");
+    Console.ReadLine();
+  }
+}
