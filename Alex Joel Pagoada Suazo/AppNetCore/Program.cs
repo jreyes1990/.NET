ee33b77517c49a8fabed81a4abab10de61405f0f
jreyes1990 <jarl1990mt@gmail.com>
2024-06-26 22:32:04 -0600

    32. Modificador public y métodos que retorna un tipo de dato

--- Alex Joel Pagoada Suazo/AppNetCore/Program.cs
+++ Alex Joel Pagoada Suazo/AppNetCore/Program.cs
@@ -5,12 +5,33 @@ class Program
   static void Main(string[] args)
   {
     var data = new Program();
-    data.metodoPrivado();
+    var edad = data.metodoIntRetorno();
+    // var name = data.metodoStringRetorno();
+    Console.WriteLine(edad);
+    Console.ReadLine();
+    // data.metodoPublico();
+    // data.metodoPrivado();
   }
 
   private void metodoPrivado()
   {
     Console.WriteLine("Hello, World!");
     Console.ReadLine();
+  }
+
+  public void metodoPublico()
+  {
+    Console.WriteLine("Hello, Jolberth!");
+    Console.ReadLine();
+  }
+
+  private string metodoStringRetorno()
+  {
+    return $"Jolberth Reyes {34}";
+  }
+
+  private int metodoIntRetorno()
+  {
+    return Convert.ToInt16("53");
   }
 }
