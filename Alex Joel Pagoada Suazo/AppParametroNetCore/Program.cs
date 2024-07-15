e23f41a93fd7cfe6c14ea1e7df54ef283e1dcfa2
jreyes1990 <jarl1990mt@gmail.com>
2024-07-14 23:31:52 -0600

    37. Como pasar un arreglo de tipo object como parámetro

---
+++ Alex Joel Pagoada Suazo/AppParametroNetCore/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AppParametroNetCore;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Object[] parametros = { "Jolberth", 34, true };
+    new Program().metodo(parametros);
+    Console.WriteLine("Hola mundo");
+    Console.ReadLine();
+  }
+
+  private void metodo(object[] parametros)
+  {
+    String nombre = (string)parametros[0];
+    int edad = (int)parametros[1];
+    bool valor = (bool)parametros[2];
+  }
+}
